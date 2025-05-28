195295e3d2a67847a13d632c1052f02f7abb18c4
Brandon Ly <brandly3257@gmail.com>
2025-05-28 09:29:01 -0400

    Added a sum method

--- Test1.java
+++ Test1.java
@@ -2,4 +2,9 @@ public class Test1 {
     public static void main(String[]args) {
         System.out.println("Hey bbg");
     }
+    
+    public static int sum(int a, int b)
+    {
+        return a+b;
+    }
 }
